fd625fbe0696e16340112ef3e273934f2f7b2e0e
c4arl0s <c.santiago.cruz@gmail.com>
2019-09-03 23:14:38 -0500

    Add comment

--- SortedMethod.playground/Contents.swift
+++ SortedMethod.playground/Contents.swift
@@ -7,6 +7,8 @@ func backward(_ stringOne: String, _ stringTwo: String) -> Bool {
 var reverseNames = names.sorted(by: backward)
 print(reverseNames)
 
+// Instead of putting the name, pass the complete function without a name
+
 var anotherReverseName = names.sorted(by: {(stringOne: String, stringTwo: String) -> Bool in
     return stringOne > stringTwo
 })
